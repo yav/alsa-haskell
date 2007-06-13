de3ce2ad667eb605ff831f4663d7cab1c145ea41
bringert <bringert@cs.chalmers.se>
2007-06-13 03:09:20 -0700

    Added examples/play.hs.
    darcs-hash:20070613100920-ea354-e02212a4dd7c9aedece280ca4b5b5c77ccb6364d.gz

---
+++ examples/play.hs
@@ -0,0 +1,28 @@
+import Sound.Alsa
+
+import System.Environment
+import System.Exit
+import System.IO
+
+bufSize :: Int
+bufSize = 4096
+
+soundFormat :: SoundFmt
+soundFormat = SoundFmt {
+                        sampleFmt   = SampleFmtLinear16BitSignedLE,
+                        sampleFreq  = 8000,
+                        numChannels = 1
+                       }
+
+main :: IO ()
+main = do args <- getArgs
+          case args of 
+            [file] -> play file
+            _      -> do hPutStrLn stderr "Usage: play <file.pcm>"
+                         exitFailure
+
+play :: FilePath -> IO ()
+play file =
+    do let source = fileSoundSource file soundFormat
+           sink   = alsaSoundSink "plughw:0,0" soundFormat
+       copySound source sink bufSize
